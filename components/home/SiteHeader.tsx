e7a08da5b4fdfbeb51ab617670e80c884ada5c41
murtazazakavi-rgb <murtazazakavi@alvazarat.org>
2026-05-13 22:15:54 +0530

    Make member login visible in header

--- components/home/SiteHeader.tsx
+++ components/home/SiteHeader.tsx
@@ -18,11 +18,16 @@ export function SiteHeader() {
         <nav className="header-nav" aria-label="Primary navigation">
           <Link href="/#experiences">Experiences</Link>
           <Link href="/#access">Access</Link>
-          <Link href="/member">Member</Link>
+          <Link href="/login">Member Login</Link>
         </nav>
-        <Link href="/request-access" className="header-action">
-          Request Access
-        </Link>
+        <div className="header-actions">
+          <Link href="/login" className="header-login">
+            Login
+          </Link>
+          <Link href="/request-access" className="header-action">
+            Request Access
+          </Link>
+        </div>
       </div>
     </header>
   );
